023fba5b99d5ca12288ec2d4fecf7caa488626b2
Ayush Majumder <ayush.majumder04@gmail.com>
2026-03-08 12:40:55 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mdeical_chatbot",
+    version="0.1.0",
+    author="Ayush Majumder",
+    author_email="ayush.majumder04@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
