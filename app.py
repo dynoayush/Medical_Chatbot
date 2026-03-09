78d9113bae75f2508a7febb154687a289dfa2e19
Ayush Majumder <ayush.majumder04@gmail.com>
2026-03-09 19:56:55 +0530

    Updated

--- app.py
+++ app.py
@@ -100,9 +100,4 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", "8080")),
-        debug=False,
-        use_reloader=False,
-    )
+    app.run(host="0.0.0.0", port=8080, debug=False, use_reloader=False)
